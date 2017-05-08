bc8d65764826b507ca1d73074a403a300fda4814
Akuilner <och5@comcast.net>
2017-05-08 00:06:56 -0400

    Exercise 2.4
    From the textbook

---
+++ src/Exercise2_4.java
@@ -0,0 +1,22 @@
+		import java.util.Scanner;
+	
+	public class Exercise2_4 {
+		public static void main(String[]args)  {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter a number in pounds: ");
+		double numberInPounds = input.nextDouble();
+			double numberInKilos = numberInPounds / 0.454;
+			
+		System.out.println(numberInPounds + " pounds is " + numberInKilos + " kilograms.");	
+		
+			
+			
+			
+			
+			
+			
+		}
+		
+}
